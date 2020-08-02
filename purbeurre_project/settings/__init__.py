d420025882f58b59476daeeb2b5fde099962f805
finevine <finevine@gmail.com>
2020-08-02 21:14:35 +0200

    allowed host

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "klhkdhgjkhveoipg5563TYfldkl!")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '46.101.143.60']
 
 # Application definition
 INSTALLED_APPS = [
