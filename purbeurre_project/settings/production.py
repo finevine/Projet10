9dd896c7ecd67786870f7b45822f2fc5ebcf354a
finevine <finevine@gmail.com>
2020-08-07 11:58:09 +0200

    secretkey default

--- purbeurre_project/settings/production.py
+++ purbeurre_project/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['46.101.143.60']
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', "Q'F?Z'L2?fbgT_-HH${TaZB4")
 
 DATABASES = {
     'default': {
