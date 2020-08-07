5a904151a27647b453b87f1ca54e33107913cce6
finevine <finevine@gmail.com>
2020-08-07 12:08:34 +0200

    travis without sentry

--- purbeurre_project/settings/travis.py
+++ purbeurre_project/settings/travis.py
@@ -1,20 +1,9 @@
 #settings for Travis
-import sentry_sdk
 import os
 from .base import *
-from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['46.101.143.60']
 
 SECRET_KEY = os.environ.get('SECRET_KEY', "Q'F?Z'L2?fbgT_-HH${TaZB4")
-
-sentry_sdk.init(
-    dsn="https://5f70900d95c14464aa5e14aaac4e0767@o429089.ingest.sentry.io/5375100",
-    integrations=[DjangoIntegration()],
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
